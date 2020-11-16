0495b63746264530f3c40a960830912bab5e69ae
d7lphins luz <d7lphins@gmail.com>
2020-11-15 22:44:50 -0300

    add react-hook-form

--- front/src/components/Formulario/index.js
+++ front/src/components/Formulario/index.js
@@ -1,58 +1,25 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import './index.css';
 
-
 function Formulario() { 
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => alert(JSON.stringify(data));
 
   return (
-
     <div className="sidebar">
-
       <div className="form"> 
-        <input
-          className="input-w100"
-          type="text"
-          id="nome"
-          name="nome"
-          placeholder="Nome do Cliente"
-        />
-
-        <input
-          className="input-w100"
-          type="text"
-          id="nome"
-          name="nome"
-          placeholder="Pesso da entrega"
-        />
-
-        <input
-          className="input-w100"
-          type="text"
-          id="nome"
-          name="nome"
-          placeholder="Endereço cliente"
-        />
-
-        <input
-          className="input-w40"
-          type="text"
-          id="nome"
-          name="nome"
-          placeholder="Latitude"
-        />
-
-        <input
-          className="input-w40"
-          type="text"
-          id="nome"
-          name="nome"
-          placeholder="Longitude"
-        />  
-
-        <button> CADASTRAR CLIENTE </button>
-        <button> RESETAR CLIENTE </button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input name="nome_do_cliente" placeholder="nome_do_cliente" ref={register} />
+          <input name="logradouro" placeholder="logradouro" ref={register} />    
+          <input name="cidade" placeholder="cidade" ref={register} />
+          <input name="pais" placeholder="pais" ref={register} />  
+          <input name="peso_em_kg" placeholder="peso_em_kg" ref={register} />
+          <input name="latitude" placeholder="lat" ref={register} />  
+          <input name="lng" placeholder="lng" ref={register} />  
+          <input type="submit" />
+        </form>
       </div>
-
     </div>
   );
 }
